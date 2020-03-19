90a849a4faa978e393e9fcbffafa4c8c369d0e48
Marina8989 <marina.mill209@gmail.com>
2020-03-18 18:58:40 -0700

    learning DOM

---
+++ js/index.js
@@ -0,0 +1,21 @@
+// Your code goes here
+const funBus = document.querySelector('.logo-heading');
+const navLink = document.querySelectorAll('.nav-link');
+
+funBus.addEventListener('mouseover', (e) => {
+    e.target.style.color = 'pink';
+});
+
+navLink.forEach(el => {
+    el.addEventListener('click', (e) => {
+        e.target.style.color = 'pink';
+        e.target.style.fontSize = '2rem';
+    });
+});
+
+navLink.forEach(el => {
+    el.addEventListener('mouseleave', (e) => {
+        e.target.style.color = 'black';
+        e.target.style.fontSize = '1.7rem';
+    })
+});
